125ecff4c96cf9e9d0825ab8b19fef0e53e68223
EdwardNun <edwardnunezventura@gmail.com>
2024-12-07 16:33:24 -0400

    Se corrigio un error con el html

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
 test('La página debe contener el texto "Hola Mundo"', () => {
-    const html = fs.readFileSync('src/index.html', 'utf8');
+    const html = fs.readFileSync('index.html', 'utf8');
     expect(html).toMatch(/Hola Mundo/);
 });
